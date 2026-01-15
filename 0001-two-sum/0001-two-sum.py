d753d4c8148587af6d5b6d4c6bd0b9d94a4ba55a
Susmitha Kusuma <127150757+SushieeK@users.noreply.github.com>
2026-01-15 17:59:33 -0600

    Time: 3 ms (50.94%), Space: 20.7 MB (7.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,54 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numToIndex = {}
+        map = {}
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in numToIndex:
-                return numToIndex[complement],i
-            numToIndex[nums[i]] =i
+            diff = target - nums[i]
+            # adding the value in key, and index in value so thata it can be found
+            if diff in map:
+                return map[diff],i
+            # adding the elemnt to the map after, so that it doesnot self count twice     
+            map[nums[i]] =i
+
+
+
+
+
+
+        # Bruteforce n2 time 
+        # n = len(nums)
+        # result = []
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if nums[i]+ nums[j] == target:
+        #             result.append(i)
+        #             result.append(j)
+        #             return result
+        # return result
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # numToIndex = {}
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     if complement in numToIndex:
+        #         return numToIndex[complement],i
+        #     numToIndex[nums[i]] =i
             
         
